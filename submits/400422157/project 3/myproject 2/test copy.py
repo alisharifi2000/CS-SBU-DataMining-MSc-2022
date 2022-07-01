b6451191a5e55800915f8848fe4c3cb2d99e757f
Mohammad Gramifar <100337437+m-gramifar@users.noreply.github.com>
2022-07-02 02:21:47 +0430

    Delete submits/400422157/project 3 directory

--- submits/400422157/project 3/myproject 2/test copy.py
+++
@@ -1,8 +0,0 @@
-from sklearn.ensemble import IsolationForest
-import numpy as np
-np.random.seed(1)
-random_data = np.random.randn(50000,2)  * 20 + 20
-
-clf = IsolationForest(max_samples=100, random_state = 1, contamination= 'auto')
-preds = clf.fit_predict(random_data)
-print(preds)
